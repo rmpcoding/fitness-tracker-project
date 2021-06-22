73537b2d7920883fbe3212d638f849aab26a8231
Robert Parsons <rmpcoding@gmail.com>
2021-06-21 21:29:27 -0700

    Backend server is now serving public files

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const htmlRoutes = require('./routes/htmlRoutes');
 const morgan = require('morgan');
 require('./db/mongoose');
 
@@ -9,6 +10,9 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json()); //allows us to access request body as req.body
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev')); //enable incoming request logging in dev mode
+app.use(express.static('./public'));
+
+app.use(htmlRoutes);
 
 app.listen(PORT, () => {
     console.log(`App is now listening on PORT: ${PORT}`);
