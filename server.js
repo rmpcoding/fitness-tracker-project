23ccb7100faf83aa8740cfaed18050f6d7ff93fb
Robert Parsons <rmpcoding@gmail.com>
2021-06-21 20:01:17 -0700

    Adds server

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const morgan = require('morgan')
+
+const app = express()
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());    //allows us to access request body as req.body
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));     //enable incoming request logging in dev mode
+
+
+app.listen(PORT, () => {
+    console.log(`App is now listening on PORT: ${PORT}`)
+})
